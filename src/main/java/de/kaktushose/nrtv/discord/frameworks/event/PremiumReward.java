7053a4d7187f502000e497863f1d30d7806a44ae
Kaktushose <faulie50@gmail.com>
2021-04-01 13:36:48 +0200

    fix premium reward

--- src/main/java/de/kaktushose/nrtv/discord/frameworks/event/PremiumReward.java
+++ src/main/java/de/kaktushose/nrtv/discord/frameworks/event/PremiumReward.java
@@ -2,6 +2,7 @@ package de.kaktushose.nrtv.discord.frameworks.event;
 
 import de.kaktushose.nrtv.discord.core.bot.Bot;
 import de.kaktushose.nrtv.discord.core.database.data.BotUser;
+import de.kaktushose.nrtv.discord.frameworks.level.shop.Item;
 import de.kaktushose.nrtv.discord.frameworks.level.shop.ItemType;
 import de.kaktushose.nrtv.discord.frameworks.level.shop.PremiumRole;
 import net.dv8tion.jda.api.entities.Member;
@@ -15,8 +16,13 @@ public class PremiumReward extends EventReward {
 
     @Override
     public void onReward(BotUser botUser, Bot bot) {
-        botUser.getItemStack().put(ItemType.PREMIUM, bot.getDatabase().getItemType(1, ItemType.PREMIUM));
-        botUser.setPremiumBuyTime(System.currentTimeMillis());
+        Item item = botUser.getItemStack().get(ItemType.PREMIUM);
+        if (item != null) {
+            if (item.getType() == 3) {
+                return;
+            }
+        }
+        bot.addUpItem(botUser, bot.getDatabase().getItemType(1, ItemType.PREMIUM));
         Member member = bot.getGuild().getMemberById(botUser.getId());
         bot.addRole(member, Bot.Roles.PREMIUM);
         bot.getDatabase().setBotUser(botUser);
