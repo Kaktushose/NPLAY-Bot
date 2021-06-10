05f33ade340b04abc12448cbaaa220485228bcc8
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2021-06-10 14:49:30 +0200

    forgot to change to production

--- src/main/java/de/kaktushose/levelbot/Bootstrapper.java
+++ src/main/java/de/kaktushose/levelbot/Bootstrapper.java
@@ -19,7 +19,7 @@ public class Bootstrapper {
         log.info("Starting bot...");
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("An unexpected error has occurred!", e));
         SpringApplication.run(Bootstrapper.class, args);
-        Levelbot levelbot = new Levelbot(Levelbot.GuildType.TESTING);
+        Levelbot levelbot = new Levelbot(Levelbot.GuildType.PRODUCTION);
         levelbot.start().indexMembers();
         startTime = System.currentTimeMillis() - startTime;
         log.info("Successfully started bot! Took {} seconds", TimeUnit.MILLISECONDS.toSeconds(startTime));
