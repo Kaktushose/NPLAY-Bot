5bebf920c9c458ecdc7a7aa13049bf8d146904c6
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2022-01-04 15:38:28 +0100

    fix rank up bug on xp bound

--- src/main/java/de/kaktushose/levelbot/database/repositories/RankRepository.java
+++ src/main/java/de/kaktushose/levelbot/database/repositories/RankRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface RankRepository extends CrudRepository<Rank, Integer> {
 
-    @Query(value = "SELECT * FROM ranks where bound < :xp order by bound desc limit 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM ranks where bound <= :xp order by bound desc limit 1", nativeQuery = true)
     Rank getRankByXp(@Param("xp") long xp);
 
 }
