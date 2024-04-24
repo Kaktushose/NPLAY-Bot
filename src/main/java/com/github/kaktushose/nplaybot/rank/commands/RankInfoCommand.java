a9b167d19a0192786e7cfd4094a7870e993e1761
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2024-04-24 21:49:07 +0200

    add context command for rank info

--- src/main/java/com/github/kaktushose/nplaybot/rank/commands/RankInfoCommand.java
+++ src/main/java/com/github/kaktushose/nplaybot/rank/commands/RankInfoCommand.java
@@ -1,16 +1,15 @@
 package com.github.kaktushose.nplaybot.rank.commands;
 
 import com.github.kaktushose.jda.commands.annotations.Inject;
-import com.github.kaktushose.jda.commands.annotations.interactions.Interaction;
-import com.github.kaktushose.jda.commands.annotations.interactions.Optional;
-import com.github.kaktushose.jda.commands.annotations.interactions.Permissions;
-import com.github.kaktushose.jda.commands.annotations.interactions.SlashCommand;
+import com.github.kaktushose.jda.commands.annotations.interactions.*;
 import com.github.kaktushose.jda.commands.data.EmbedCache;
 import com.github.kaktushose.jda.commands.dispatching.interactions.commands.CommandEvent;
 import com.github.kaktushose.nplaybot.Database;
 import com.github.kaktushose.nplaybot.permissions.BotPermissions;
 import com.github.kaktushose.nplaybot.rank.model.UserInfo;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.commands.Command;
 
 @Interaction
 @Permissions(BotPermissions.USER)
@@ -22,11 +21,19 @@ public class RankInfoCommand {
     private EmbedCache embedCache;
 
     @SlashCommand(value = "rank", isGuildOnly = true, desc = "Zeigt die Kontoinformationen zu einem User an")
-    public void onCommand(CommandEvent event, @Optional Member member) {
+    public void onRankInfo(CommandEvent event, @Optional Member member) {
         var target = member == null ? event.getMember() : member;
         UserInfo userInfo = database.getRankService().getUserInfo(target);
 
         var embed = userInfo.nextRank().isPresent() ? "rankInfo" : "rankInfoMax";
         event.reply(embedCache.getEmbed(embed).injectValues(userInfo.getEmbedValues(target)));
+    }
+
+    @ContextCommand(value = "Kontoinformation abrufen", type = Command.Type.USER, isGuildOnly = true, ephemeral = true)
+    public void onContextRankInfo(CommandEvent event, User user) {
+        UserInfo userInfo = database.getRankService().getUserInfo(user);
+
+        var embed = userInfo.nextRank().isPresent() ? "rankInfo" : "rankInfoMax";
+        event.reply(embedCache.getEmbed(embed).injectValues(userInfo.getEmbedValues(user)));
     }
 }
