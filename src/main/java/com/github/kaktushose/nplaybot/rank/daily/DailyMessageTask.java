95ec4aaa4e6ef9b7c7515fd98be289e9365e3f83
Kaktushose <matti.psk@gmail.com>
2024-07-10 18:07:39 +0200

    fix daily message task

--- src/main/java/com/github/kaktushose/nplaybot/rank/daily/DailyMessageTask.java
+++ src/main/java/com/github/kaktushose/nplaybot/rank/daily/DailyMessageTask.java
@@ -2,7 +2,9 @@ package com.github.kaktushose.nplaybot.rank.daily;
 
 import com.github.kaktushose.nplaybot.Bot;
 import com.github.kaktushose.nplaybot.scheduler.ScheduledTask;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.UserSnowflake;
+import net.dv8tion.jda.api.exceptions.ErrorHandler;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,14 +13,11 @@ public class DailyMessageTask {
     @ScheduledTask(period = 24, unit = TimeUnit.HOURS, startAtMidnight = true)
     public void onSendDailyMessages(Bot bot) {
         bot.getDatabase().getRankService().getDailyRankInfos().forEach(user ->
-                bot.getJda().retrieveUserById(user.id())
-                        .flatMap(User::openPrivateChannel)
-                        .flatMap(channel ->
-                                channel.sendMessage(
-                                        bot.getEmbedCache().getEmbed(user.nextRank().isPresent() ? "rankInfo" : "rankInfoMax")
-                                                .injectValues(user.getEmbedValues(channel.getUser(), true)).toMessageCreateData()
-                                )
-                        ).queue()
+                bot.getGuild().retrieveMember(UserSnowflake.fromId(user.id())).queue(member ->
+                        member.getUser().openPrivateChannel().flatMap(channel -> channel.sendMessage(
+                                bot.getEmbedCache().getEmbed(user.nextRank().isPresent() ? "rankInfo" : "rankInfoMax")
+                                        .injectValues(user.getEmbedValues(member.getUser(), true)).toMessageCreateData()
+                        )).queue(), new ErrorHandler().ignore(ErrorResponse.UNKNOWN_MEMBER))
         );
     }
 
